e2c961224b6bf8efa0f5a51961b5ea1a34857255
xinyuz1996 <xzhang959@wisc.edu>
2022-02-13 23:09:12 -0500

    Update ex1.R

--- ex1.R
+++ ex1.R
@@ -3,3 +3,15 @@ i=3
 print(i)
 cherry = lh
 ar(cheery)
+
+library(Amelia)
+?amelia()
+data(africa)
+summary(africa)
+str(africa)
+m=1
+a.out <- amelia(x = africa, cs = "country", ts = "year", logs = "gdp_pc", m=1)
+summary(a.out)
+plot(a.out)
+a.out
+a.out$imputations$imp1
