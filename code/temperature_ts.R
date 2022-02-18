bd2ac3601539cd3c74e8f72e4bb25ad4d49d7a0b
xinyuz1996 <xzhang959@wisc.edu>
2022-02-18 00:48:25 -0500

    Update temperature_ts.R

--- code/temperature_ts.R
+++ code/temperature_ts.R
@@ -1,3 +1,11 @@
 # time series forecasting
 
+cherry <- read.csv("../data/kyoto.csv")
+# cherry <- read.csv("../data/washingtondc.csv") %>% 
+#   bind_rows(read.csv("../data/liestal.csv")) %>% 
+#   bind_rows(read.csv("../data/kyoto.csv"))
 
+summary(cherry)
+cherry %>% 
+  group_by(location) %>% 
+  slice_tail(n = 3)
